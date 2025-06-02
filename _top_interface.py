1ab9d0e3cdad354bb47e84afc5d9ef4896ee7778
EddieMoualek2003 <58135479+EddieMoualek2003@users.noreply.github.com>
2025-06-02 16:03:14 +0100

    Update _top_interface.py

--- _top_interface.py
+++ _top_interface.py
@@ -1,4 +1,6 @@
 import subprocess
+import threading
+import keyboard
 
 from lights_out.lights_out_top import *
 from dice_game.dice_game_top import *
@@ -18,15 +20,40 @@ def debug_main():
     choice = int(input("Enter a game option: "))
     gameArray[choice-1][1]()
 
-# Step 3: Main entry
-if __name__ == "__main__":
-    # debug_main()
-    for game in gameArray:
-        print(f"\t\t {game[0]}")
-    try:
-        choice = int(input("Enter a game option: "))
-        gameArray[choice-1][1]()
-    except:
-        print("Invalid Option")
+def run_with_hotkey():
+    stop_event = threading.Event()
 
-#comments
+    # Register the hotkey
+    keyboard.add_hotkey('esc', stop_event.set)
+
+    # Start the main loop
+    main_loop(stop_event)
+
+def main_loop(stop_event):
+    # Message to the user on how to leave the game
+    print("Press 'ESC' to come back to the main menu when done")
+    # Message to the user on the actual game modes.
+    for game in gameArray:
+            print(f"\t\t {game[0]}")
+    # Let the user pick their game of choice
+    choice = int(input("Enter a game option: "))
+    # The game will be repeated until
+    while not stop_event.is_set():
+        # Run the game until the hot-key is pressed.
+        gameArray[choice-1][1]()
+        
+        
+
+
+# # Step 3: Main entry
+# if __name__ == "__main__":
+#     # debug_main()
+#     for game in gameArray:
+#         print(f"\t\t {game[0]}")
+#     try:
+#         choice = int(input("Enter a game option: "))
+#         gameArray[choice-1][1]()
+#     except:
+#         print("Invalid Option")
+
+# #comments
